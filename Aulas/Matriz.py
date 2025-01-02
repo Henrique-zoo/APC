97f817a0d8acd3b92b4c99ffaa7bb7c7da2fb2ac
Henrique-zoo <morcelleshenrique@gmail.com>
2025-01-01 23:08:41 -0300

    opa

--- Aulas/Matriz.py
+++
@@ -1,7 +0,0 @@
-n, m = map(int, input().split())
-matriz = []
-for _ in range(0, n):
-    linha = [int(x) for x in input().split()]
-    matriz.append(linha)
-for i in range(0, m):
-    print(matriz[i])
--- Aulas/OrdenandoLista.py
+++
@@ -1,9 +0,0 @@
-lista = input().split()
-n = len(lista)
-for i in range(n):
-    for j in range(i+1, n):
-        if lista[j] < lista[i]:
-            aux = lista[i]
-            lista[i] = lista[j]
-            lista[j] = aux
-print(lista)
